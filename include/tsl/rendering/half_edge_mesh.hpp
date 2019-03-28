1267e99498f6a52278ccd3373684d0f6077bb8b7
Johan M. von Behren <johan@vonbehren.eu>
2019-03-28 13:00:06 +0100

    Add functions to generate picked buffer for edges and vertices

--- include/tsl/rendering/half_edge_mesh.hpp
+++ include/tsl/rendering/half_edge_mesh.hpp
@@ -1,11 +1,18 @@
 #ifndef TSL_RENDERING_HALF_EDGE_MESH_HPP
 #define TSL_RENDERING_HALF_EDGE_MESH_HPP
 
+#include <functional>
+
 #include <tsl/gl_buffer.hpp>
 #include <tsl/geometry/half_edge_mesh.hpp>
 #include <tsl/rendering/picking_map.hpp>
 
+using std::reference_wrapper;
+
 namespace tsl {
+
+vector<uint8_t> get_picked_edges_buffer(const half_edge_mesh& mesh, const vector<reference_wrapper<const picking_element>>& picked);
+vector<uint8_t> get_picked_vertices_buffer(const half_edge_mesh& mesh, const vector<reference_wrapper<const picking_element>>& picked);
 
 gl_buffer get_edges_buffer(const half_edge_mesh& mesh, picking_map& picking_map);
 gl_buffer get_vertices_buffer(const half_edge_mesh& mesh, picking_map& picking_map);
