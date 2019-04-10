117ac429f441945562e1328b355e84c4bf4ded51
Johan M. von Behren <johan@vonbehren.eu>
2019-04-10 16:02:25 +0200

    Add tests for `stable_vector::operator[]`

--- test/src/attrmaps/stable_vector_tests.cpp
+++ test/src/attrmaps/stable_vector_tests.cpp
@@ -27,6 +27,32 @@ TEST_F(StableVectorTest, PushCreatesCopy) {
     EXPECT_EQ(21, temp.val);
 }
 
+TEST(IndexOperatorTest, AssignCreatesCopy) {
+    dummy temp = {42};
+    dummy temp2 = {21};
+    stable_vector<test_handle, dummy> vector(10, temp);
+
+    // Test if the default value is changable.
+    test_handle handle(0);
+    auto& val = (*vector.get(handle)).get();
+    EXPECT_EQ(42, val.val);
+    val.val = 41;
+    EXPECT_EQ(41, vector[handle].val);
+
+    // Test if the index operator in combination with value assignment creates a copy of the value.
+    test_handle handle1(1);
+    EXPECT_EQ(42, vector[handle1].val);
+    auto& new_val = (vector[handle1] = temp2);
+    EXPECT_EQ(21, vector[handle1].val);
+    temp2.val = 20;
+    EXPECT_EQ(20, temp2.val);
+    EXPECT_EQ(21, vector[handle1].val);
+    EXPECT_EQ(21, new_val.val);
+
+    new_val.val = 19;
+    EXPECT_EQ(19, vector[handle1].val);
+}
+
 }
 
 #pragma clang diagnostic pop
