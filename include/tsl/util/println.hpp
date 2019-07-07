7a4a9b799ad4726f4cb810846fbfe113cddeb5cc
Johan M. von Behren <johan@vonbehren.eu>
2019-07-07 18:23:50 +0200

    Refactor `println` to accept only one parameter

--- include/tsl/util/println.hpp
+++ include/tsl/util/println.hpp
@@ -15,8 +15,8 @@ namespace tsl
 /**
  * @brief Prints the given string formated like `fmt::print` but adds a "\n" after it.
  */
-template <typename... Args>
-inline void println(const char* format_str, const Args& ... args) {
+template <typename S, typename... Args>
+inline void println(const S& format_str, const Args& ... args) {
     print(format_str, args...);
     print("\n");
 }
