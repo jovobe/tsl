4bae12dea4e1d8e8eb8db42fd3f28e27b2d09365
Johan M. von Behren <johan@vonbehren.eu>
2019-07-13 14:47:22 +0200

    Add docs to the rendering module.

--- include/tsl/rendering/picking_map.hpp
+++ include/tsl/rendering/picking_map.hpp
@@ -16,28 +16,55 @@ using std::optional;
 
 namespace tsl {
 
+/**
+ * @brief Represents the type of a picked object.
+ */
 enum class object_type {
     vertex,
     edge,
     face
 };
 
+/**
+ * @brief POD type to represent an element which was picked inside the editor.
+ */
 struct picking_element {
+    /// Type of the picked element.
     object_type type;
+    /// Handle of the picked element.
     base_handle<index> handle;
 
     picking_element(object_type type, base_handle<index> handle) : type(type), handle(handle) {}
     bool operator<(const picking_element& r) const;
 };
 
+/**
+ * @brief A map, which maps a unique id to a picked element. All elements in a scene will be given a unique id, to be
+ * identified later by this id.
+ */
 class picking_map {
 private:
+    /// Id of the next element which will be added.
     uint32_t next;
+    /// Map to store relation between id and elements.
     unordered_map<uint32_t, picking_element> map;
 public:
     picking_map() : next(1) {}
+
+    /**
+     * @brief Adds an object to the picking map with the given type and the given handle and returns the id of the
+     * added element.
+     */
     uint32_t add_object(object_type type, base_handle<index> handle);
+
+    /**
+     * @brief Returns a picking element from the map, identified by the given id.
+     */
     optional<picking_element> get_object(uint32_t idx) const;
+
+    /**
+     * @brief Clears the map and removes all entries.
+     */
     void clear();
 };
 
--- include/tsl/rendering/tmesh.hpp
+++ include/tsl/rendering/tmesh.hpp
@@ -13,11 +13,30 @@ using std::set;
 namespace tsl
 {
 
+/**
+ * @brief Creates a buffer with picking ids for faces from the given vector of
+ * regular grids and a set of picked elements.
+ */
 vector<uint8_t> get_picked_faces_buffer(const vector<regular_grid>& faces, const set<picking_element>& picked);
+
+/**
+ * @brief Creates a buffer with picking ids for edges from the given tmesh and a set of picked elements.
+ */
 vector<uint8_t> get_picked_edges_buffer(const tmesh& mesh, const set<picking_element>& picked);
+
+/**
+ * @brief Creates a buffer with picking ids for vertices from the given tmesh and a set of picked elements.
+ */
 vector<uint8_t> get_picked_vertices_buffer(const tmesh& mesh, const set<picking_element>& picked);
 
+/**
+ * @brief Generates an OpenGL ready buffer for picking edges from the given tmesh and picking map.
+ */
 gl_buffer get_edges_buffer(const tmesh& mesh, picking_map& picking_map);
+
+/**
+ * @brief Generates an OpenGL ready buffer for picking vertices from the given tmesh and picking map.
+ */
 gl_buffer get_vertices_buffer(const tmesh& mesh, picking_map& picking_map);
 
 }
